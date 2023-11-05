553825216d28665a7ea2932d609df298883e5d3a
MinJaeSon <mjsonsj@gmail.com>
2023-11-06 05:19:45 +0900

    [add] 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Start from './pages/Start';
+import Test from './pages/Test';
+import Result from './pages/Result';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Start />} />
+        <Route path="/test" element={<Test />} />
+        <Route path="/result" element={<Result />} />
       </Routes>
     </>
   );
---
+++ src/pages/Test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Test = () => {
+  return <div>테스트화면</div>;
+};
+
+export default Test;
