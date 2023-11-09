b14ad71b8862d637a163be2d7337cb1f43b94115
MinJaeSon <mjsonsj@gmail.com>
2023-11-10 05:34:06 +0900

    [chore] 마지막 답변까지 저장해서 전달하기 성공

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import './Test.css';
 import logo from './assets/logo.svg';
 import ProgressBar from '../components/ProgressBar';
 import TestItem from '../components/TestItem';
@@ -17,7 +18,14 @@ const Test = () => {
   // 선택된 답변이 추가되거나 변경될 때마다 콘솔 출력
   useEffect(() => {
     console.log(answers);
-  }, [answers]);
+
+    // 마지막 질문의 답변을 선택했는지 확인 (answers가 완전히 업데이트 된 후 넘길 수 있도록)
+    if (answers.length === testData.length) {
+      setTimeout(() => {
+        navigate('/loading', { state: { arrayProps: answers } });
+      }, 0);
+    }
+  }, [answers, navigate]);
 
   // 답변 선택 시 이벤트 핸들러
   const onHandleAnswer = (answer) => {
@@ -28,13 +36,13 @@ const Test = () => {
       return nextAnswer;
     });
 
-    // 마지막 질문이 아니라면 다음 문제로 넘어가도록
-    if (currentQuestionIndex < testData.length - 1) {
-      setCurrentQuestionIndex((prev) => prev + 1);
-    } else {
-      // 마지막 질문이라면 로딩 페이지로
-      navigate('/loading', answers); // 선택된 답변 데이터 같이 넘기기
-    }
+    // 질문 끝났는지 체크하기 위해 비동기로 처리
+    setTimeout(() => {
+      // 마지막 질문이 아니라면 다음 문제로 넘어가도록
+      if (currentQuestionIndex < testData.length - 1) {
+        setCurrentQuestionIndex((prev) => prev + 1);
+      }
+    }, 0);
   };
 
   // 이전 문제로 되돌리는 이벤트 핸들러
@@ -57,6 +65,7 @@ const Test = () => {
         />
       </TopBar>
       <TestItem
+        style={{ padding: '20px' }}
         data={testData[currentQuestionIndex]}
         selectedAnswer={answers[currentQuestionIndex]}
         onAnswer={onHandleAnswer}
